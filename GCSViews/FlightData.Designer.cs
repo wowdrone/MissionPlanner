54b54a4fba3e59b2028e357b8d326aa79b18e98f
wowdrone <cumonyo@naver.com>
2025-03-19 17:33:45 +0900

    ceo v2

--- GCSViews/FlightData.Designer.cs
+++ GCSViews/FlightData.Designer.cs
@@ -2,7 +2,7 @@ using System.Windows.Forms;
 
 namespace MissionPlanner.GCSViews
 {
-    partial class FlightData
+    public partial class FlightData : System.Windows.Forms.UserControl//partial class FlightData
     {
         private System.ComponentModel.IContainer components = null;
 
