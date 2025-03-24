9fe1caca71406f91598b8dea67afdd028dffab73
wowdrone <cumonyo@naver.com>
2025-03-24 12:06:38 +0900

    gdalconfiguration 오류나서 파일추가

---
+++ ExtLibs/GDAL/GdalConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using OSGeo.GDAL;
+using OSGeo.OGR;
+
+public static class GdalConfiguration
+{
+    private static bool _configuredGdal = false;
+    private static bool _configuredOgr = false;
+
+    public static void ConfigureGdal()
+    {
+        if (!_configuredGdal)
+        {
+            Gdal.AllRegister();
+            _configuredGdal = true;
+        }
+    }
+
+    public static void ConfigureOgr()
+    {
+        if (!_configuredOgr)
+        {
+            Ogr.RegisterAll();
+            _configuredOgr = true;
+        }
+    }
+}
