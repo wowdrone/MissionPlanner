ea50eb773cb2cb5ef84d4c901de53bc09a494212
wowdrone <cumonyo@naver.com>
2025-04-04 16:06:53 +0900

    커) 고도정보 콘솔에찍기

--- HJSCUSTOM/MyCustomView.cs
+++ HJSCUSTOM/MyCustomView.cs
@@ -13,11 +13,23 @@ namespace MissionPlanner.HJSCUSTOM
 {
     public partial class MyCustomView : MyUserControl
     {
+        string battrey;
+        int voltage;
+
+
         public MyCustomView()
         {
             InitializeComponent();
+            Timer Timer = new System.Windows.Forms.Timer();
+            Timer.Interval = 500;
+            Timer.Tick += new EventHandler(MAVLINK_SENDER);
+            Timer.Start();
         }
-
+        private void MAVLINK_SENDER(object sender, EventArgs e)
+        {
+            float custom_altitude = MainV2.comPort.MAV.cs.alt;
+            Console.WriteLine($"고도 : {custom_altitude} m");
+        }
         private void button12_Click(object sender, EventArgs e)
         {
             MessageBox.Show("배터리잔량");
